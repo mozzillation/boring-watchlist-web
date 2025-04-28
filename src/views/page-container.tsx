6514c3d9bfc036e7f4e55af3279db1585f6e7ad3
Giuliano Mozzillo <giulianomozzillo@live.it>
2025-04-28 20:43:20 +0200

    Test to link Linear issue to BW-12

--- src/views/page-container.tsx
+++ src/views/page-container.tsx
@@ -2,9 +2,7 @@
 
 type PageContainerProps = React.PropsWithChildren
 
-const PageContainer: React.FC<PageContainerProps> = (props) => {
-  const { children } = props
-
+const PageContainer: React.FC<PageContainerProps> = ({ children }) => {
   return (
     <section className={`max-w-3xl m-auto w-full h-screen`}>{children}</section>
   )
