0e1112d29ae3cbc1249fd1d162b9ca1781363f39
Giuliano Mozzillo <giulianomozzillo@live.it>
2025-04-30 02:27:07 +0200

    feat: ref BW-52 - husky initial setup
    Add Husky for pre-commit linting and formatting

---
+++ commitlint.config.js
@@ -0,0 +1,10 @@
+const Configuration = {
+  /*
+   * Resolve and load @commitlint/config-conventional from node_modules.
+   * Referenced packages must be installed
+   */
+  extends: ['@commitlint/config-conventional'],
+  //   plugins: ['@commitlint/plugin-regex'],
+}
+
+export default Configuration
