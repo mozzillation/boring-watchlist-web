6b6d34a7f8607c531e8166a46f3171a8dab1c4b8
Giuliano Mozzillo <giulianomozzillo@live.it>
2025-04-30 02:59:51 +0200

    feat: commitlint rules - closes BW-52
    Force the usage of Linear's magic words and issue IDs in commit-msg command

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,10 +1,30 @@
-const Configuration = {
+module.exports = {
   /*
    * Resolve and load @commitlint/config-conventional from node_modules.
    * Referenced packages must be installed
    */
   extends: ['@commitlint/config-conventional'],
-  //   plugins: ['@commitlint/plugin-regex'],
-}
 
-export default Configuration
+  rules: {
+    'task-id-required': [2, 'always'],
+    'body-match': [
+      2,
+      'always',
+      /(?:close|closes|closed|closing|fix|fixes|fixed|fixing|resolve|resolves|resolved|resolving|complete|completes|completed|completing|ref|references|part of|related to|contributes to|towards)\s+[A-Z]+-\d+/,
+    ],
+  },
+  plugins: [
+    'commitlint-plugin-regex-match',
+    {
+      rules: {
+        'task-id-required': ({ raw }) => {
+          const pattern = /\bBW-\d+\b/
+
+          return pattern.test(raw)
+            ? [true]
+            : [false, 'Commit must include a Linear issue ID e.g. BW-123']
+        },
+      },
+    },
+  ],
+}
