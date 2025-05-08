6b4ac7cfb2f82805c23999dd4e5fe5de40b356a9
Giuliano Mozzillo <giulianomozzillo@live.it>
2025-05-08 13:36:23 +0200

    feat: enhance TV details view with loading states for images and add watchlist button, part of BW-29

--- src/views/tv-details-view.tsx
+++ src/views/tv-details-view.tsx
@@ -1,15 +1,21 @@
 'use client'
 
+import WatchlistButton from '@/components/media/watchlist-button'
 import PageContainer from '@/components/navigation/page-container'
 import { TV } from '@/lib/tmdb/schema'
 import { buildTMDBImageUrl, formatSeasonCount } from '@/lib/tmdb/utils/helpers'
+import { cn } from '@/lib/utils'
 import Image from 'next/image'
+import { useState } from 'react'
 
 type TVDetailsViewProps = {
   tv: TV
 }
 
 const TVDetailsView: React.FC<TVDetailsViewProps> = ({ tv }) => {
+  const [isBackdropLoaded, setBackdropLoaded] = useState(false)
+  const [isPosterLoaded, setPosterLoaded] = useState(false)
+
   const backdrop = buildTMDBImageUrl(tv.backdrop_path, 'w154')
   const poster = buildTMDBImageUrl(tv.poster_path, 'w342')
 
@@ -22,46 +28,61 @@ const TVDetailsView: React.FC<TVDetailsViewProps> = ({ tv }) => {
           className={`relative w-full h-auto bg-muted overflow-hidden pointer-events-none select-none py-6 px-4 sm:px-6`}
         >
           {backdrop && (
-            <Image
-              src={backdrop}
-              fill={true}
-              alt={tv.name}
-              className={`blur-3xl absolute inset-0 object-cover`}
-              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-            />
+            <>
+              <Image
+                src={backdrop}
+                fill={true}
+                alt={tv.name}
+                className={cn(
+                  `absolute inset-0 object-cover transition-all opacity-0`,
+                  isBackdropLoaded && `opacity-100`,
+                )}
+                onLoadingComplete={() => setBackdropLoaded(true)}
+                sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+              />
+              <div
+                className={`absolute inset-0 backdrop-blur-3xl backdrop-brightness-125`}
+              />
+            </>
           )}
 
           <div
-            className={`max-w-3xs w-full aspect-[5/7] h-auto relative rounded overflow-hidden shadow-2xl bg-black/10`}
+            className={`max-w-[33vw] sm:max-w-3xs w-full aspect-[5/7] h-auto relative rounded-md overflow-hidden shadow-2xl bg-black/10 border-white/10 border`}
           >
             {poster && (
               <Image
                 src={poster}
                 fill={true}
                 alt={tv.name}
-                className={`object-cover`}
+                className={cn(
+                  `object-cover transition-all opacity-0`,
+                  isPosterLoaded && `opacity-100`,
+                )}
                 priority
+                onLoadingComplete={() => setPosterLoaded(true)}
                 sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
               />
             )}
           </div>
         </picture>
-        <div className={`py-4 px-4 sm:px-6`}>
-          <header className={`flex flex-col gap-1`}>
-            <div
-              className={`w-full flex flex-row flex-wrap gap-2 text-sm text-muted-foreground`}
-            >
-              <div>{new Date(tv.first_air_date).getFullYear().toString()}</div>
-              <div>{formatSeasonCount(tv.number_of_seasons)}</div>
+        <div className={`py-4 px-4 sm:px-6 space-y-4`}>
+          <header className={`flex flex-col gap-4`}>
+            <div className={`flex flex-col gap-1`}>
+              <div
+                className={`w-full flex flex-row flex-wrap gap-1 text-sm text-muted-foreground`}
+              >
+                <div>
+                  {new Date(tv.first_air_date).getFullYear().toString()}
+                </div>
+                <div>{formatSeasonCount(tv.number_of_seasons)}</div>
+              </div>
+              <h1 className="scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-3xl text-balance">
+                {tv.name}
+              </h1>
             </div>
-            <h1 className="scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-3xl text-balance">
-              {tv.name}
-            </h1>
-            {/* {tv. && (
-              <h2 className="scroll-m-20 text-xl font-semibold tracking-tight text-secondary-foreground">
-                {movie.tagline}
-              </h2>
-            )} */}
+            <div className="flex flex-row">
+              <WatchlistButton />
+            </div>
           </header>
           <main>
             {tv.overview && (
